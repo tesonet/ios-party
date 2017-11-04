4dda7fbbf2ad430702ea9061937853f91f518be9
Vitalii Budnik <inekrich@me.com>
2017-11-04 09:45:04 +0200

    Project init

---
+++ TesonetServersList/Sources/TesonetServersList/Controllers/ViewController.swift
@@ -0,0 +1,13 @@
+//
+//  ViewController.swift
+//  TesonetServersList
+//
+//  Created by Vitalii Budnik on 11/4/17.
+//  Copyright © 2017 Tesonet. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+	
+}
---
+++ TesonetServersList/Sources/TesonetServersList/Supporting Files/AppDelegate.swift
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.swift
+//  TesonetServersList
+//
+//  Created by Vitalii Budnik on 11/4/17.
+//  Copyright © 2017 Tesonet. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+	
+	var window: UIWindow?
+	
+	func application(
+		_ application: UIApplication,
+		didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?)
+		-> Bool
+	{
+		// Override point for customization after application launch.
+		return true
+	}
+	
+}
