b472e5f8845d22500ac7c27b43326fa5d7a8e173
tesosa <tesosa@tesosa.lt>
2017-11-26 23:15:07 +0200

    Display fetched servers

--- tesosa/tesosa/View controllers/Servers list view controller/ServersListDataSource.swift
+++ tesosa/tesosa/View controllers/Servers list view controller/ServersListDataSource.swift
@@ -1,13 +1,27 @@
 import UIKit
 
+let ServerCellIdentifier = "ServerCell"
+
 class ServerListDataSource: NSObject, UITableViewDataSource {
-    var servers = [Server]()
+    private var servers = [Server]()
+    
+    func update(serversDTO: Servers) {
+        servers = serversDTO.servers
+    }
+    
+    // MARK: - UITableViewDataSource
     
     public func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 0
+        return servers.count
     }
 
     public func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        return UITableViewCell()
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: ServerCellIdentifier, for: indexPath) as? ServerCell,
+            servers.count > indexPath.row else {
+                assertionFailure("Servers list data source setup wrong")
+                return UITableViewCell(style: .default, reuseIdentifier: nil)
+        }
+        cell.update(server: servers[indexPath.row])
+        return cell
     }
 }
--- tesosa/tesosa/View controllers/Servers list view controller/ServersListViewController.swift
+++ tesosa/tesosa/View controllers/Servers list view controller/ServersListViewController.swift
@@ -9,28 +9,47 @@ class ServersListViewController: UIViewController {
     
     // MARK: - View life cycle
     
+    override func viewDidLoad() {
+        setupTableView()
+    }
+    
     override func viewDidAppear(_ animated: Bool) {
         if appearingFirstTime {
             appearingFirstTime = false
-            loaderController.modalPresentationStyle = .custom
-            loaderController.modalTransitionStyle = .crossDissolve
-            present(loaderController, animated: true, completion: nil)
-            apiService.fetchServers(
-                success: { [weak self] servers in
-                    self?.loaderController.dismiss(animated: true, completion: nil)
-                },
-                failure: { [weak self] error in
-                    self?.loaderController.dismiss(animated: true, completion: nil)
-                    UIAlertController.presentErrorAlert(error: error)
-                }
-            )
+            loadData()
         }
-        
     }
     
     // MARK: - Actions
+    
     @IBAction func logoutButtonTapped() {
         TokenService.removeToken()
         dismiss(animated: true, completion: nil)
+    }
+    
+    // MARK: - Private
+    
+    private func setupTableView() {
+        tableView.dataSource = dataSource
+        tableView.register(UINib(nibName: ServerCellIdentifier, bundle: nil), forCellReuseIdentifier: ServerCellIdentifier)
+        tableView.estimatedRowHeight = 50
+        tableView.rowHeight = UITableViewAutomaticDimension
+    }
+    
+    private func loadData() {
+        loaderController.modalPresentationStyle = .custom
+        loaderController.modalTransitionStyle = .crossDissolve
+        present(loaderController, animated: true, completion: nil)
+        apiService.fetchServers(
+            success: { [weak self] servers in
+                self?.loaderController.dismiss(animated: true, completion: nil)
+                self?.dataSource.update(serversDTO: servers)
+                self?.tableView.reloadData()
+            },
+            failure: { [weak self] error in
+                self?.loaderController.dismiss(animated: true, completion: nil)
+                UIAlertController.presentErrorAlert(error: error)
+            }
+        )
     }
 }
