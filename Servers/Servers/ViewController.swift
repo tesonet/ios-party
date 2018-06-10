0105cb3c0443552ef94746acc26ad3a7e678ee1a
Rimantas Lukosevicius <rimantas@keyspace.lt>
2018-06-10 11:53:34 +0300

    Add Xcode project

---
+++ Servers/Servers/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Servers
+//
+//  Created by Rimantas Lukosevicius on 10/06/2018.
+//  Copyright © 2018 Rimantas Lukosevicius. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
