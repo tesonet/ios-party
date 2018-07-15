a8c7400aa608d456483d68c56e416c7a77cf2262
Robert <robertasbaronas@hotmail.co>
2018-07-15 19:29:09 +0100

    add login view controller

---
+++ Tesonet/Tesonet/Login/LoginViewController.swift
@@ -0,0 +1,23 @@
+import UIKit
+
+class LoginViewController: UIViewController {
+    
+    @IBOutlet fileprivate weak var usernameTextField: UITextField!
+    @IBOutlet fileprivate weak var passwordTextField: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        self.navigationController?.setNavigationBarHidden(true, animated: animated)
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        self.navigationController?.setNavigationBarHidden(false, animated: animated)
+    }
+    
+}
+
