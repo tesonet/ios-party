4a07bcbd0315c28cfb1be2a30e2622fed33adc4f
Mindaugas Jucius <mindaugas.jucius@icloud.com>
2018-07-26 17:48:59 +0300

    Add flow coordinator, remove storyboard

---
+++ Testio/Testio/AppFlowCoordinator.swift
@@ -0,0 +1,22 @@
+//
+//  AppFlowCoordinator.swift
+//  Testio
+//
+//  Created by Mindaugas on 26/07/2018.
+//  Copyright © 2018 Mindaugas Jucius. All rights reserved.
+//
+
+import UIKit
+
+class AppFlowCoordinator: UINavigationController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        startFlow()
+    }
+    
+    func startFlow() {
+        
+    }
+    
+}
