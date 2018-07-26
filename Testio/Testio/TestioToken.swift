0cceeee63f06914527899b08c9e266c3c2a9e815
Mindaugas Jucius <mindaugas.jucius@icloud.com>
2018-07-26 17:20:04 +0300

    Add Token, User entities

---
+++ Testio/Testio/TestioToken.swift
@@ -0,0 +1,15 @@
+//
+//  TestioToken.swift
+//  Testio
+//
+//  Created by Mindaugas on 26/07/2018.
+//  Copyright © 2018 Mindaugas Jucius. All rights reserved.
+//
+
+import Foundation
+
+struct TestioToken: Codable {
+    
+    let token: String
+    
+}
