42cf31c1fcefade363a6384b1d78a90629b56139
Mindaugas Jucius <mindaugas.jucius@icloud.com>
2018-07-27 11:19:24 +0300

    Add padded UITextField, .strings file, colors struct

---
+++ Testio/Testio/TestioUIConstants.swift
@@ -0,0 +1,29 @@
+//
+//  TestioUIConstants.swift
+//  Testio
+//
+//  Created by Mindaugas on 27/07/2018.
+//  Copyright © 2018 Mindaugas Jucius. All rights reserved.
+//
+
+import UIKit
+
+struct Colors {
+    
+    static let actionColor = UIColor.rgbColor(red: 159, green: 214, blue: 51, alpha: 1)
+    
+}
+
+extension UIColor {
+    
+    static func rgbColor(red: Int, green: Int, blue: Int, alpha: CGFloat) -> UIColor {
+        let redComponent = CGFloat(red) / 255
+        let greenComponent = CGFloat(green) / 255
+        let blueComponent = CGFloat(blue) / 255
+        
+        return UIColor.init(red: redComponent,
+                            green: greenComponent,
+                            blue: blueComponent,
+                            alpha: alpha)
+    }
+}
