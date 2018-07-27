061f22c7aadd404e0dcc09d316086d38c17bd140
Mindaugas Jucius <mindaugas.jucius@icloud.com>
2018-07-27 08:18:17 +0300

    Add login flow classes

---
+++ Testio/Testio/Login/LoginViewController.swift
@@ -0,0 +1,18 @@
+//
+//  LoginViewController.swift
+//  Testio
+//
+//  Created by Mindaugas on 26/07/2018.
+//  Copyright © 2018 Mindaugas Jucius. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+}
---
+++ Testio/Testio/Login/LoginViewModel.swift
@@ -0,0 +1,13 @@
+//
+//  LoginViewModel.swift
+//  Testio
+//
+//  Created by Mindaugas on 27/07/2018.
+//  Copyright © 2018 Mindaugas Jucius. All rights reserved.
+//
+
+import Foundation
+
+class LoginViewModel {
+
+}
