6898393a6da0a9195bec98321bc27cc0ec7f0066
Lorenas <lorenas.bartkus@nfq.lt>
2019-02-09 12:16:47 +0200

    Initial project setup with Pods.

---
+++ Testio/Testio/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Testio
+//
+//  Created by lbartkus on 09/02/2019.
+//  Copyright © 2019 lbartkus. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
