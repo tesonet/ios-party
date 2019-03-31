6de55e2d1d999a2ea66194a3921bf1467671b2bc
Paulius Cesekas <cheskapac@gmail.com>
2019-04-01 02:29:27 +0300

    Setup extra pods for code validy - SwiftGen for code generation - SwiftLint for code checks

---
+++ TesonetHomework/Domain/Entries/Authorization/Login.swift
@@ -0,0 +1,11 @@
+// Created by Paulius Cesekas on 01/04/2019.
+
+import Foundation
+
+public struct Login {
+    public let token: String
+    
+    public init(token: String) {
+        self.token = token
+    }
+}
---
+++ TesonetHomework/TesonetHomework/Scenes/Authorization/Login/LoginViewController.swift
@@ -0,0 +1,6 @@
+// Created by Paulius Cesekas on 01/04/2019.
+
+import UIKit
+
+class LoginViewController: UIViewController {
+}
