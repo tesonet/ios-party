f23c1e87377611d3bba8d4c249840fb018897e85
Justas <justas.liola@mediapark.lt>
2019-04-06 10:18:18 +0300

    Added Xcode project

---
+++ Mobile App/Mobile App/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Mobile App
+//
+//  Created by Justas Liola on 06/04/2019.
+//  Copyright © 2019 Justin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
