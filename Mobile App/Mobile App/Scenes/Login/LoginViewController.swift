69c7d84914ab0699be64bca828212617bc10c07b
Justas <justas.liola@mediapark.lt>
2019-04-06 11:35:26 +0300

    Seperated Login and Servers UI parts

--- Mobile App/Mobile App/Scenes/Login/LoginViewController.swift
+++ Mobile App/Mobile App/Scenes/Login/LoginViewController.swift
@@ -8,13 +8,12 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+final class LoginViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
     }
-
 
 }
 
