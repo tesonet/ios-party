0a8508ae8b4bb6a3f152981f72168c79d8ce5b43
darius <darius.vilcinskis@gmail.com>
2019-06-08 15:29:23 +0300

    Replace storyboard with custom VC

--- ios-party/ios-party/AppDelegate.swift
+++ ios-party/ios-party/AppDelegate.swift
@@ -8,7 +8,13 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        
+        window = UIWindow(frame: UIScreen.main.bounds)
+        
+        let viewController = LoginViewController()
+        window?.rootViewController = viewController
+        window?.makeKeyAndVisible()
+        
         return true
     }
 
