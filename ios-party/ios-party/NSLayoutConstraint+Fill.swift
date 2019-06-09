b38adaf9ab9690e6e88465281e2c6fea1aba1f96
darius <darius.vilcinskis@gmail.com>
2019-06-09 09:55:55 +0300

    Add constraint fill extension

---
+++ ios-party/ios-party/NSLayoutConstraint+Fill.swift
@@ -0,0 +1,13 @@
+import UIKit
+
+extension NSLayoutConstraint {
+    
+    static func fill(view parent: UIView, with child: UIView) {
+        NSLayoutConstraint.activate([
+            child.topAnchor.constraint(equalTo: parent.topAnchor),
+            child.bottomAnchor.constraint(equalTo: parent.bottomAnchor),
+            child.leadingAnchor.constraint(equalTo: parent.leadingAnchor),
+            child.trailingAnchor.constraint(equalTo: parent.trailingAnchor)
+        ])
+    }
+}
