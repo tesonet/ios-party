f88d51ed60c614675c5ac99863218e967959bfa7
Darius Janavicius <darius.j@ito.lt>
2019-07-18 00:23:27 +0300

    add auth token storage

---
+++ Party/Common/Services/apiClient/AuthTokenStorage.swift
@@ -0,0 +1,48 @@
+//
+//  AuthTokenStorage.swift
+//  Party
+//
+//  Created by Darius Janavicius on 17/07/2019.
+//  Copyright © 2019 tesonet. All rights reserved.
+//
+
+import Foundation
+
+class AuthTokenStorage {
+    
+    // MARK: Constants
+    
+    static let authTokenKey: String = "authTokenKey"
+    
+    // MARK: Dependencies
+    
+    let storage: UserDefaults
+    
+    // MARK: - Init
+    
+    init(storage: UserDefaults = UserDefaults.standard) {
+        self.storage = storage
+    }
+    
+    // MARK: - Public Methods
+    
+    func store(_ token: AuthToken?, forKey key: String = authTokenKey) {
+        guard let token = token else {
+            return
+        }
+        let data = try? JSONEncoder().encode(token)
+        storage.set(data, forKey: key)
+        storage.synchronize()
+    }
+    
+    func retrieve(forKey key: String = authTokenKey) -> AuthToken? {
+        guard let data = storage.data(forKey: key) else {
+            return nil
+        }
+        return try? JSONDecoder().decode(AuthToken.self, from: data)
+    }
+    
+    func clear(forKey key: String = authTokenKey) {
+        store(nil, forKey: key)
+    }
+}
