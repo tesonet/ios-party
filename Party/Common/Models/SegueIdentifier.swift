4ce6e4cff85b333166cf452fb4a0e2409397dfe0
Darius Janavicius <darius.j@ito.lt>
2019-07-17 16:04:51 +0300

    add convenient way for managing segue identifiers

---
+++ Party/Common/Models/SegueIdentifier.swift
@@ -0,0 +1,28 @@
+//
+//  SegueIdentifier.swift
+//  Party
+//
+//  Created by Darius Janavicius on 17/07/2019.
+//  Copyright © 2019 tesonet. All rights reserved.
+//
+
+import Foundation
+
+/// A structure for storing segue identifiers.
+struct SegueIdentifier {
+    var rawValue: String
+}
+
+extension SegueIdentifier {
+    
+    init(_ rawValue: String) {
+        self.rawValue = rawValue
+    }
+}
+
+extension SegueIdentifier: Equatable {
+    
+    static func ==(lhs: SegueIdentifier, rhs: SegueIdentifier) -> Bool {
+        return lhs.rawValue == rhs.rawValue
+    }
+}
