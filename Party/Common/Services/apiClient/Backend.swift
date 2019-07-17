84798b3a6f915ff29f77683b8a39a55c31be3e4a
Darius Janavicius <darius.j@ito.lt>
2019-07-17 18:49:11 +0300

    add backend struct for base url

---
+++ Party/Common/Services/apiClient/Backend.swift
@@ -0,0 +1,14 @@
+//
+//  Backend.swift
+//  Party
+//
+//  Created by Darius Janavicius on 17/07/2019.
+//  Copyright © 2019 tesonet. All rights reserved.
+//
+
+import Foundation
+
+struct Backend {
+    /// The base URL of the backend.
+    static let baseUrl = URL(string: "http://playground.tesonet.lt")!
+}
