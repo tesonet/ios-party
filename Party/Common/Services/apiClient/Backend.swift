08e2b4ee11e3f7bba7fec2380cf37c8a6075aeb1
Darius Janavicius <darius.j@ito.lt>
2019-07-17 18:53:36 +0300

    add endpoint structure

--- Party/Common/Services/apiClient/Backend.swift
+++ Party/Common/Services/apiClient/Backend.swift
@@ -12,3 +12,23 @@ struct Backend {
     /// The base URL of the backend.
     static let baseUrl = URL(string: "http://playground.tesonet.lt")!
 }
+
+struct Endpoint {
+    
+    // MARK: - RawRepresentable
+    
+    var rawValue: String
+    
+    init?(rawValue: String) {
+        self.rawValue = rawValue
+    }
+}
+
+extension Endpoint {
+    
+    /// Generates token. Http method: POST
+    static let tokens = "/v1/tokens"
+    
+    /// Loads list of servers. Http method: GET
+    static let servers = "/v1/servers"
+}
