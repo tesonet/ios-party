b2415f6ef7ac162bfa0472fe3dfd289bd8db95bf
Edvinas <edvpra@gmail.com>
2019-08-08 01:11:34 +0300

    initial commit

---
+++ project/Sources/MainViewController.swift
@@ -0,0 +1,18 @@
+//
+//  MainViewController.swift
+//  XibApp
+//
+//  Created by Edvinas on 2019-07-24.
+//  Copyright © 2019 Edvinas. All rights reserved.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+}
