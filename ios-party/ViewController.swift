c4f66effc8d69c8bd7ed4441dcaf913e19945e90
Juozas Valancius <juozas.valancius@icloud.com>
2020-02-22 10:51:54 +0200

    Add Xcode Project from Template

---
+++ ios-party/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ios-party
+//
+//  Created by Joseph on 2/22/20.
+//  Copyright © 2020 Juozas Valancius. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
