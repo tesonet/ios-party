1b0f2cc02bc8d60c3596934aa9b7d21aeaec3997
AlexanderVecherov <aliaksandr.vecharou@itechart-group.com>
2020-02-25 01:59:28 +0300

    moved resources to resources folder

--- Tesonet/Tesonet/AppDelegate.swift
+++ Tesonet/Tesonet/AppDelegate.swift
@@ -2,16 +2,11 @@
 //  AppDelegate.swift
 //  Tesonet
 //
-//  Created by Александр on 2/24/20.
-//  Copyright © 2020 Alexander. All rights reserved.
-//
 
 import UIKit
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
-
-
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
--- Tesonet/Tesonet/SceneDelegate.swift
+++ Tesonet/Tesonet/SceneDelegate.swift
@@ -2,16 +2,12 @@
 //  SceneDelegate.swift
 //  Tesonet
 //
-//  Created by Александр on 2/24/20.
-//  Copyright © 2020 Alexander. All rights reserved.
-//
 
 import UIKit
 
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
     var window: UIWindow?
-
 
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         // Use this method to optionally configure and attach the UIWindow `window` to the provided UIWindowScene `scene`.
@@ -47,7 +43,5 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // Use this method to save data, release shared resources, and store enough scene-specific state information
         // to restore the scene back to its current state.
     }
-
-
 }
 
