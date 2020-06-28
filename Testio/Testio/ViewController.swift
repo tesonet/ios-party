e2cd4fb0a2dbcbbea450037a640906a01c77139e
Ernestas Šeputis <ernestas@jumsoft.com>
2020-06-28 11:12:49 +0300

    initial commit

---
+++ Testio/Testio/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Testio
+//
+//  Created by Ernestas Šeputis on 6/28/20.
+//  Copyright © 2020 Ernestas Šeputis. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
