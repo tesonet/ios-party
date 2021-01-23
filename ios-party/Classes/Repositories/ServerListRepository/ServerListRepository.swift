a01b21e1ce2b9b701fe6b1a8e6403b29063e5272
Ergin Bilgin <ergin.bilgin@telesoftas.com>
2021-01-23 23:33:29 +0200

    ServerListRepository: initial file creation

---
+++ ios-party/Classes/Repositories/ServerListRepository/ServerListRepository.swift
@@ -0,0 +1,16 @@
+//
+//  ServerListRepository.swift
+//  ios-party
+//
+//  Created by Ergin Bilgin on 2021-01-23.
+//
+
+import Foundation
+
+protocol ServerListRepositoryInterface {
+    
+}
+
+class ServerListRepository: ServerListRepositoryInterface {
+    
+}
