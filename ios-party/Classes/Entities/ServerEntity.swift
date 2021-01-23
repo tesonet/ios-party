291e97499e06984f51fbe4f808720ddf38cef886
Ergin Bilgin <ergin.bilgin@telesoftas.com>
2021-01-23 23:23:13 +0200

    implement ServerEntity

--- ios-party/Classes/Entities/ServerEntity.swift
+++ ios-party/Classes/Entities/ServerEntity.swift
@@ -10,5 +10,40 @@ import Foundation
 class ServerEntity {
     
     // MARK: - Declarations
+    var name: String
+    var distance: Int
     
+    // MARK: - Methods
+    // MARK: - Initialization
+    init?(from dictionary: [String: Any]) {
+        guard let name = dictionary["name"] as? String else {
+            log("ERROR! Dictionary does not have name: \(dictionary)")
+            return nil
+        }
+        
+        guard let distance = dictionary["distance"] as? Int else {
+            log("ERROR! Dictionary does not have distance: \(dictionary)")
+            return nil
+        }
+        
+        self.name = name
+        self.distance = distance
+    }
+    
+    // MARK: - Helpers
+    static func serverListFrom(dictList: [[String: Any]]) -> [ServerEntity] {
+        
+        var serverList: [ServerEntity] = []
+        for serverDict in dictList {
+            
+            guard let server = ServerEntity(from: serverDict) else {
+                log("ERROR! Could not parse ServerEntity from dictionary: \(serverDict)")
+                continue
+            }
+            
+            serverList.append(server)
+        }
+        
+        return serverList
+    }
 }
