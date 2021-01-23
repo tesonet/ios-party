f2ed750b4a7ba2251c19b6f557675642751c2624
Ergin Bilgin <ergin.bilgin@telesoftas.com>
2021-01-23 21:05:38 +0200

    LoginDataModel: implement login request

--- ios-party/Classes/ViewControllers/Login/DataModel/LoginDataModel.swift
+++ ios-party/Classes/ViewControllers/Login/DataModel/LoginDataModel.swift
@@ -6,6 +6,7 @@
 //
 
 import Foundation
+import Alamofire
 
 protocol LoginDataModelDelegate: AnyObject {
     func loginDataModel(didStartLogin dataModel: LoginDataModelInterface)
@@ -21,7 +22,6 @@ class LoginDataModel: LoginDataModelInterface {
     
     // MARK: - Declarations
     weak var delegate: LoginDataModelDelegate?
-    var operationQueue = OperationQueue()
     var isLoading = false
 
     // MARK: - Dependencies
@@ -30,10 +30,6 @@ class LoginDataModel: LoginDataModelInterface {
     // MARK: - Methods
     init(delegate: LoginDataModelDelegate) {
         self.delegate = delegate
-    }
-    
-    deinit {
-        operationQueue.cancelAllOperations()
     }
     
     func login(withUsername username: String, password: String) {
@@ -45,9 +41,64 @@ class LoginDataModel: LoginDataModelInterface {
         isLoading = true
         delegate?.loginDataModel(didStartLogin: self)
         
-        // FIXME: implement login operation
-        let operation = Operation()
+        var parameterDict: [String: String] = [:]
+        parameterDict["username"] = username
+        parameterDict["password"] = password
         
-        operationQueue.addOperation(operation)
+        let request: DataRequest = AF.request("https://playground.tesonet.lt/v1/tokens",
+                                              method: .post,
+                                              parameters: parameterDict,
+                                              encoding: JSONEncoding.default)
+            .validate(statusCode: 200..<300)
+        
+        request.responseJSON(completionHandler: { [weak self] response in
+            switch response.result {
+            case .success(let data):
+                self?.didFinishLoginRequest(responseData: data)
+                
+            case .failure(let error):
+                self?.didFailLoginRequest(error: error)
+            }
+        })
+    }
+    
+    func didFinishLoginRequest(responseData: Any) {
+        isLoading = false
+        
+        guard let responseDict = responseData as? [String: Any] else {
+            log("ERROR! Could not parse response dictionary from response data: \(responseData)")
+            delegate?.loginDataModel(didFailLogin: self)
+            return
+        }
+        
+        guard let token = responseDict["token"] as? String else {
+            log("ERROR! Could not parse token from response dictionary: \(responseDict)")
+            delegate?.loginDataModel(didFailLogin: self)
+            return
+        }
+        
+        authorization.setToken(token: token)
+        delegate?.loginDataModel(didFinishLogin: self)
+    }
+    
+    func didFailLoginRequest(error: AFError) {
+        isLoading = false
+        
+        if error.responseCode == 401 {
+            log("ERROR! Login failed with code 401")
+            delegate?.loginDataModel(didFailLogin: self)
+        } else {
+            log("Login request failed with error: \(error)")
+            delegate?.loginDataModel(didFailLogin: self)
+        }
+    }
+    
+    func parseAccessToken(responseData: Any) -> String? {
+        guard let responseData = responseData as? [String: Any],
+              let token = responseData["token"] as? String else {
+            return nil
+        }
+        
+        return token
     }
 }
