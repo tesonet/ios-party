ee950322de6c86de2be0de64ad8010dab278953d
Samet <samet@local>
2021-02-28 00:07:18 +0100

    add transition animation

--- Party-iOS/Party-iOS/Coordinator.swift
+++ Party-iOS/Party-iOS/Coordinator.swift
@@ -32,8 +32,18 @@ final class Coordinator {
     private func showMain() {
         let listVC = ListViewController()
         listVC.coordinator = self
-        window?.rootViewController = UINavigationController(rootViewController: listVC)
-        window?.makeKeyAndVisible()
+        let navigationController = UINavigationController(rootViewController: listVC)
+        let completion: (Bool) -> Void = { [weak self] _ in
+            guard let self = self else { return }
+            self.window?.rootViewController = navigationController
+            self.window?.makeKeyAndVisible()
+        }
+        guard let currentView = window?.rootViewController?.view else {
+            completion(true)
+            return
+        }
+        UIView.transition(from: currentView, to: navigationController.view, duration: 0.5, options: .transitionCrossDissolve, completion: completion)
+ 
     }
     
     private func showLogin() {
